ce95c9388b0e4629f91cd54991284e8fd369621a
Carlos Adir <carlos.adir.leite@gmail.com>
2024-05-16 18:22:26 +0000

    refactor: remove unneded files

--- src/math.hpp
+++
@@ -1,8 +0,0 @@
-
-typedef unsigned short ushort;
-typedef double parameter;
-
-namespace Math{
-    ushort binom(const ushort n, const ushort i);
-    parameter bezier(const parameter u, const ushort p, const ushort i);
-};
