1b7e492d72ecd00cdb61f9a7af1ebd18cbee45f6
Carlos Adir <carlos.adir.leite@gmail.com>
2025-11-08 17:00:36 +0100

    update loggers

--- include/cshape/loggers.h
+++ include/cshape/loggers.h
@@ -7,34 +7,126 @@
 #include <map>
 #include <vector>
 #include <memory>
-class Logger {
-private:
-    static std::map<const std::string, std::shared_ptr<Logger>> instances;
 
-public:
-    Logger(const std::string& loggerName);  // Make it private
-    const std::string name;
-    static std::shared_ptr<Logger> getInstance(const std::string& filename);
+// Enum to represent log levels
+enum class LogLevel { DEBUG, INFO, WARNING, ERROR, CRITICAL };
+
+
+typedef struct LogMessage
+{
+    const time_t time;
+    const LogLevel level;
+    const std::string logger;
+    const std::string message;
+} LogMessage;
+
+
+const char* levelToString(LogLevel level){
+    switch (level)
+    {
+        case LogLevel::DEBUG:
+            return "DEBUG";
+        case LogLevel::INFO:
+            return "INFO";
+        case LogLevel::WARNING:
+            return "WARNING";
+        case LogLevel::CRITICAL:
+            return "CRITICAL";
+        case LogLevel::ERROR:
+            return "ERROR";
+        default:
+            throw std::invalid_argument("Out of options");
+    }
+}
+
+
+std::ostream &operator<<(std::ostream &os, const LogMessage &obj){
+    tm* timeinfo = localtime(&obj.time);
+    char timestamp[20];
+    strftime(timestamp, sizeof(timestamp),
+             "%Y-%m-%d %H:%M:%S", timeinfo);
+
+    os << '[' << timestamp << "] ";
+    os << levelToString(obj.level) << ": ";
+    os << '(' << obj.logger << ')';
+    os << obj.message;
+    return os;
+}
+
+//
+//
+//
+//
+//
+//
+//   HANDLERS
+//
+//
+//
+//
+//
+//
+
+class IHandler 
+{
+    public:
+        virtual void write(const LogMessage message) const = 0;
 };
 
-// Why do we need it ? It's already defined inside the class
-std::map<const std::string, std::shared_ptr<Logger>> Logger::instances;
-
-std::shared_ptr<Logger> Logger::getInstance(const std::string& loggerName)
+class TransmiterHandler : public IHandler
 {
-    std::cout << "Getting instance: " << loggerName.size() << ": '" << loggerName << "'" << std::endl;
-    // Check if instance exists
-    if (instances.count(loggerName)) {
-        return instances.at(loggerName);
-    }
+    private:
+        std::vector<IHandler> handlers = {};
+    public:
+        TransmiterHandler();
+        void write(const LogMessage message) const;
+};
+
+//
+//
+//
+//
+//
+//
+//   LOGGER
+//
+//
+//
+//
+//
+//
+
+class Logger {
+private:
+    static std::map<const std::string, Logger> instances;
+    Logger(const std::string& loggerName);  // Now private
     
-    // Create new instance
-    std::shared_ptr<Logger> logger = std::make_shared<Logger>(loggerName);
-    instances.insert({loggerName, logger});
-    return logger;
-}
+public:
+    const std::string name;
+    static Logger& getInstance(const std::string& filename);
+    void log(const LogLevel level, const std::string &filename) const;
+};
+
 
 Logger::Logger(const std::string& loggerName) : name(loggerName)
 {
     std::cout << "Created instance: '" << loggerName << "' at " << (size_t)&(*this) << std::endl;
+
 }
+
+std::map<const std::string, Logger> Logger::instances;
+
+Logger& Logger::getInstance(const std::string& loggerName)
+{
+    std::cout << "Getting instance: " << loggerName.size() << ": '" << loggerName << "'" << std::endl;
+    if (!instances.count(loggerName))
+        instances.insert({loggerName, Logger(loggerName)});
+    return instances.at(loggerName);
+}
+
+
+void Logger::log(const LogLevel level, const std::string& message) const
+{
+    const LogMessage logMessage = {time(0), level, name, message};
+}
+
